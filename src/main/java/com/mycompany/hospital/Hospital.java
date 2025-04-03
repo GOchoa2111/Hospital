a6156fde117304cc2e7d7de6b090455cb80d7e84
GOchoa2111 <gochoa.2111@gmail.com>
2025-04-02 21:45:09 -0600

    Proyecto Hospital

---
+++ src/main/java/com/mycompany/hospital/Hospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospital;
+
+/**
+ *
+ * @author Gary,Carlos,Bryan,JuanJose
+ */
+public class Hospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hola team");
+    }
+}
